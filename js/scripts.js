d42fcb2377ca5fd4a1a7d7f9dc9316ae933a391e
Tyson Lackey <lackeyt90@gmail.com>
2020-06-11 13:36:41 -0700

    add handling for subtraction behavior
    co-authored-by: Erich Richter <erich.richter@gmail.com>
    Co-authored-by: Kate Skorija <kateskorija@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,24 @@
 $(document).ready(function() {
   //Business Logic
 
+  // constructs integer into roman numberal notation
   function arrayBuilder(int) {
     let array = []
-    if (int >= 5){
+    if (int === "9") {
+      array.push("1s", "10")
+    } else if (int >= 5 && int < "9") {
       array.push("5")
-      if (int - 5 === 4){
-        array.push("1", "1", "1", "1")
-      } else if (int - 5 === 3){
+      if (int - 5 === 3){
         array.push("1", "1", "1")
       } else if (int - 5 === 2){
         array.push("1", "1")
       } else if (int - 5 === 1) {
         array.push("1")
       };
-    } else if (int < 5) {
-      if (int === "4"){
-        array.push("1", "1", "1", "1")
-      } else if (int === "3"){
+    } else if (int === "4"){
+      array.push("1s", "5")
+    } else if (int < 4) {
+      if (int === "3"){
         array.push("1", "1", "1")
       } else if (int === "2"){
         array.push("1", "1")
@@ -25,57 +26,88 @@ $(document).ready(function() {
         array.push("1")
       };
     };
-    return array
+    return array;
   };
 
-  function translator(int) {
-    let onesArray = []
-    let tensArray = []
-    let hundredsArray = []
-    let thousandsArray = []
+  //converts integers into roman numeral value
+  function converter(array){
 
-    posCount = 0
+    newArray =[];
+
+    for (const element of array){
+      if (element === "1000"){
+        newArray.push("M");
+      } else if (element === "500"){
+        newArray.push("D");
+      } else if (element === "100"){
+        newArray.push("C");
+      } else if (element === "50"){
+        newArray.push("L");
+      } else if (element === "10"){
+        newArray.push("X");
+      } else if (element === "5"){
+        newArray.push("V");
+      } else if (element === "1" || element === "1s"){
+        newArray.push("I");
+      };
+    };
+    return newArray;
+  };
+
+  // translates integer into roman numerals
+  function translator(int) {
+    let onesArray = [];
+    let tensArray = [];
+    let hundredsArray = [];
+    let thousandsArray = [];
+
+    posCount = 0;
     for (i=int.length - 1; i >= 0; i--){
       if (posCount === 0) {
-        //ones
         onesArray = arrayBuilder(int[i]);
       } else if (posCount === 1) {
-        //tens
         for (const element of arrayBuilder(int[i])){
-          tensArray.push(element + "0")
+          if (element === "1s") {
+            tensArray.push(element);
+          } else {
+            tensArray.push(element + "0");
+          };
         };
       } else if (posCount === 2) {
-        //hundreds
         for (const element of arrayBuilder(int[i])){
-          hundredsArray.push(element + "00")
+          if (element === "1s") {
+            hundredsArray.push(element);
+          } else {
+            hundredsArray.push(element + "00");
+          };
         };
       } else if (posCount === 3) {
-        //thousands
         for (const element of arrayBuilder(int[i])){
-          thousandsArray.push(element + "000")
+          if (element === "1s") {
+            thousandsArray.push(element);
+          } else {
+            thousandsArray.push(element + "000");
+          };
         };
       };
       posCount += 1
     }; 
+    
+    let thousandsArrayNew = converter(thousandsArray)
+    let hundredsArrayNew = converter(hundredsArray)
+    let tensArrayNew = converter(tensArray)
+    let onesArrayNew = converter(onesArray)
 
-    output = thousandsArray + hundredsArray + tensArray + onesArray
-
-    return converter(output)
+    let output = (thousandsArrayNew.concat(hundredsArrayNew, tensArrayNew, onesArrayNew)).join("")
+    return output
   };
 
-  function converter(array){
-    newArray =[]
-    for (const element of array){
-      if (element === "1000"){
-        newArray.push("M")
-      }
-    }
-    return newArray
-  }
-
-  console.log(translator("3774"))
 
   //User Interface
+
+  const input = $("#number").val()
+
+  $(".output").text(translator(input))
 
 
 });
